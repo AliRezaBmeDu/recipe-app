aa5dd20cfee712bdb5cb3dcabaa0af14712862cc
AliReza <ali.reza.buet@gmail.com>
2023-11-19 13:16:27 +0600

    Add private methods to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,15 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
     def index
         @recipes = Recipe.all
     end
 
     def show
+    end
+
+    def public_recipe
+        @recipe_public = Recipe.where(public: true)
     end
 
     def new
@@ -19,4 +24,15 @@ class RecipesController < ApplicationController
         render :new
         end
     end
+
+    private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  end
+
 end
