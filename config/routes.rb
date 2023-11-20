82aa134d49f2042bcf3d14b80b17f345edf27058
AliReza <ali.reza.buet@gmail.com>
2023-11-20 12:29:49 +0600

    Fix public recipe

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/foods', to: 'foods#index', as: :foods
   get '/recipes', to: 'recipes#index', as: :recipes
   get '/recipes/:recipe_id', to: 'recipes#show', as: :recipe_specific
-  get '/public_recipes', to: 'recipe#public_recipe', as: :public_recipe
+  get '/public_recipes', to: 'recipes#public_recipe', as: :public_recipe
   get '/general_shopping_list', to: 'shopping#general_list', as: :general_shopping_list
   root "users#index"
 end
