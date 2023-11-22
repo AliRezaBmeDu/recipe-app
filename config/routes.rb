fd786dbc6c8a66b9ed214d3c8e97d461247d37e1
AliReza <ali.reza.buet@gmail.com>
2023-11-22 12:11:45 +0600

    Update the devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth'
+  
   get '/users', to: 'users#index', as: :users
   get '/foods', to: 'foods#index', as: :foods
   get '/recipes', to: 'recipes#index', as: :recipes
@@ -6,4 +8,12 @@ Rails.application.routes.draw do
   get '/public_recipes', to: 'recipes#public_recipe', as: :public_recipe
   get '/general_shopping_list', to: 'shopping#general_list', as: :general_shopping_list
   root "users#index"
+
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new", as: :sign_in_root
+  end
 end
