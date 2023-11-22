23df886c9958b82bc0fd0692c9f786dc33bb1765
sheezu900 <shahzamanlive2@gmail.com>
2023-11-22 18:53:12 +0500

    add functionality to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,25 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @foods = current_user.foods
+  end
+
+  def show
+    @food = current_user.foods.find(params[:id])
+  end
+
+  def new
+    @food = current_user.foods.build
+  end
+
+  def create
+    @food = current_user.foods.build(food_params)
+
+    if @food.save
+      redirect_to @food, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
+  end
 end
