5fbff03d636de9d4bc5179b973ca0ff2e2297a08
sheezu900 <shahzamanlive2@gmail.com>
2023-11-22 18:54:25 +0500

    add functionality to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -22,4 +22,31 @@ class FoodsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @food = current_user.foods.find(params[:id])
+  end
+
+  def update
+    @food = current_user.foods.find(params[:id])
+
+    if @food.update(food_params)
+      redirect_to @food, notice: 'Food was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @food = current_user.foods.find(params[:id])
+    @food.destroy
+
+    redirect_to foods_url, notice: 'Food was successfully destroyed.'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
