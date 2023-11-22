32709d88d22e71f9bc172475fd30657997995891
AliReza <ali.reza.buet@gmail.com>
2023-11-22 12:09:24 +0600

    Update user and recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,14 +2,5 @@ class Recipe < ApplicationRecord
     belongs_to :user
 
     private
-
-  def convert_times_to_seconds
-    self.preparation_time = convert_string_to_time(preparation_time)
-    self.cooking_time = convert_string_to_time(cooking_time)
-  end
-
-  def convert_string_to_time(time_string)
-    hours, minutes, seconds = time_string.split(':').map(&:to_i)
-    hours.hours + minutes.minutes + seconds.seconds
-  end
+    
 end
