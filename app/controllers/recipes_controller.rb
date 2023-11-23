19e20f8103d757bf87ceab1fe0ee50af6baf68f1
AliReza <ali.reza.buet@gmail.com>
2023-11-23 08:51:01 +0600

    Fix the foods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
     before_action :set_recipe, only: [:show]
 
     def index
-        @recipes = Recipe.all
+        @recipes = current_user.recipes
     end
 
     def show
