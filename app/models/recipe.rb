dea99d0b76bc5af88d894fca22fe298e5e3b9540
AliReza <ali.reza.buet@gmail.com>
2023-11-25 01:24:57 +0600

    Add recipe model spec

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,10 @@ class Recipe < ApplicationRecord
     has_many :recipe_foods, dependent: :destroy
     has_many :foods, through: :recipe_foods
 
+    validates :name, presence: true
+    validates :preparation_time, presence: true
+    validates :cooking_time, presence: true
+    validates :description, presence: true
     def change_public_status
         update(public: !public)
     end
